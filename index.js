61eb0919fa467db76745320ef43e763a183ecbca
Bruna Aguiar <baguiar861@gmail.com>
2023-10-26 19:45:37 -0300

    Desafio concluido com sucesso

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("ola, mundo")
+let SaldoVitorias = Calculadora(102,2)
+let nivel = NivelRank(SaldoVitorias)
+
+console.log(SaldoVitorias, nivel)
+
+
+function Calculadora(vitorias, derrrotas){
+    let resultado = vitorias - derrrotas
+    return resultado
+
+}
+
+function NivelRank(SaldoVitorias){
+    if(SaldoVitorias < 10){
+        let rank = "Ferro"
+    }else if(SaldoVitorias > 10 && SaldoVitorias < 20){
+        rank = "Bronze"
+    }else if(SaldoVitorias > 20 && SaldoVitorias < 50){
+        rank = "Prata   "
+    }else if(SaldoVitorias > 50 && SaldoVitorias < 80){
+        rank = "Ouro"
+    }else if(SaldoVitorias > 80 && SaldoVitorias < 90){
+        rank = "Diamante"
+    }else if(SaldoVitorias > 90 && SaldoVitorias < 100){
+        rank = "Lendário"
+    }else if(SaldoVitorias >= 100){
+        rank = "Imortal"
+    }
+    
+    
+    return rank
+
+}
